60d7805063570a577c1c4254bd4285db04e00b14
yelenka <yelenagorunov@gmail.com>
2016-09-18 22:34:17 +0300

    Update gameCreator.service.ts

--- app/game/gameCreator.service.ts
+++ app/game/gameCreator.service.ts
@@ -2,26 +2,30 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GameCreatorService {
+    //default game data
     private gameData = {
         turn: 'x',
         winningsX: 0,
         winningsO: 0,
         winner: '',
-        isStarted: false
+        isStarted: false,
+        dimension: 3,
+        widthOfCell: 0
     };
     private board = [];
-
+    //create board by dimension
     createBoard(dimension :number) {
         this.board = [];
         for (let row = 0; row < dimension; row++) {
             this.board[row] = [];
             for (let col = 0; col < dimension; col++) {
-                this.board[row][col] = null;
+                this.board[row][col] = {};
             }
         }
         this.gameData.isStarted = true;
         return this.board;
     }
+    //get gamedata
     getGameData(){
         return this.gameData;
     }
@@ -31,6 +35,7 @@ export class GameCreatorService {
         this.board = [];
         this.gameData.isStarted = false;
     }
+    //check board for winning rows
     checkBoard(row, col){
         var columnArray = this.board.map(function(value,index) { return value[col]; });
         var leftToRight = this.board.map((row, index) => row[index]);
@@ -38,14 +43,19 @@ export class GameCreatorService {
 
         //check arrays for winner
         if(this.checkArray(this.board[row]) || this.checkArray(columnArray) || this.checkArray(leftToRight) || this.checkArray(rightToLeft)){
+            //set winner current turn
             this.gameData.winner = this.gameData.turn;
+            //count winnings for current turn
             this.gameData.turn === 'x' ? this.gameData.winningsX++ : this.gameData.winningsO++;
+            //empty the board
             this.board = [];
             console.log('Winner in row is ' + this.gameData.turn);
         }
     }
+    //checks arrays for winning row
     checkArray(array){
-        if(array.every(s => s === this.gameData.turn)){
+        if(array.every(s => s.turn === this.gameData.turn)){
+            array.every(s => s.isWinningCell = true);
             return true;
         }
         return false;
