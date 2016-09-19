825ed4c09017e09ceda06b0bf089caa5704313dc
yelenka <yelenagorunov@gmail.com>
2016-09-19 10:45:27 +0300

    Update board.component.ts

--- app/game/board/board.component.ts
+++ app/game/board/board.component.ts
@@ -1,23 +1,22 @@
-import { Component } from '@angular/core';
-import { GameCreatorService } from 'app/game/gameCreator.service.ts'
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Player } from 'app/game/player.ts';
 
 @Component({
     selector: '.board',
-    inputs: ['board'],
+    inputs: ['board', 'gameData'],
     templateUrl: 'app/game/board/board.html',
     styleUrls: ['app/game/board/board.css']
 })
 export class BoardComponent {
     public board = [];
-    gameData = this._gameCreatorService.getGameData();
-
-    constructor(private _gameCreatorService: GameCreatorService) {    }
+    public gameData;
+    @Output() cellSelected = new EventEmitter();
 
     onSelect(row :number, col: number){
         if (this.board[row][col].turn)
             return;
         this.board[row][col] = {turn: this.gameData.turn};
-        this._gameCreatorService.checkBoard(row, col);
-        this.gameData.turn = this.gameData.turn === 'o' ? 'x' : 'o';
+        this.cellSelected.emit({row:row, col:col});
+        this.gameData.turn = this.gameData.turn === Player.o ? Player.x : Player.o;
     }
 }
